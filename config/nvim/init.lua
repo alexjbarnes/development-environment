6926f90da49c8c76f7e9cfdec2d8ade9f96b239c
alex barnes <alexb151@gmail.com>
2025-06-17 19:33:39 +0100

    Add fish thrme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,3 +9,7 @@ require("mason-lspconfig").setup({
   ensure_installed = { "lua_ls", "dockerls", "gopls", "rust_analyzer", "terraformls", "jsonls", "yamlls" },
   automatic_installation = true,
 })
+
+-- Set relative line numbers off
+vim.opt.number = true
+vim.opt.relativenumber = false
