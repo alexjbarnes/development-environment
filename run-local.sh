e2f1475ae7b3d05fc246eb5e722f58ef344e13b0
alex barnes <alexb151@gmail.com>
2025-06-17 05:55:04 +0100

    Updates

--- run-local.sh
+++ run-local.sh
@@ -1,3 +1,14 @@
 cd "$(dirname "$(readlink -f "$0")")"
 
-docker run --env-file .env --privileged -v /proc:/proc -v /sys:/sys -v ~/repos:/root/repos -v /root/.local/share/fish/fish_history:/root/.local/share/fish/fish_history -v ~/repos/development-environment/config:/root/.config -v ~/.gitconfig:/root/.gitconfig -v ~/repos/development-environment/nvim:/root/.local/share/nvim -v /var/run/docker.sock:/var/run/docker.sock -it ghcr.io/alexjbarnes/development-environment:latest fish
+docker run \
+  --env-file .env \
+  --privileged \
+  -v /proc:/proc \
+  -v /sys:/sys \
+  -v ~/repos:/root/repos \
+  -v /root/.local/share/fish/:/root/.local/share/fish/ \
+  -v ~/repos/development-environment/config:/root/.config \
+  -v ~/.gitconfig:/root/.gitconfig \
+  -v ~/repos/development-environment/nvim:/root/.local/share/nvim \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -it ghcr.io/alexjbarnes/development-environment:latest fish
